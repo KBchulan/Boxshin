155d6d16bcad65f6e83de58d8b35858f62a45ccf
KBchulan <18737519552@163.com>
2024-08-25 18:41:01 +0800

    更新生命类基类

--- 代码空间/VS2022/game_name/enemy.h
+++ 代码空间/VS2022/game_name/enemy.h
@@ -1,1 +1,51 @@
-#pragma once
+#ifndef _ENEMTY_H_
+#define _ENEMTY_H_
+
+#include"animation.h"
+
+class Enemy {
+
+public:
+	Enemy() = default;
+	~Enemy() = default;
+
+	void set_atlas(Atlas* atlas) {
+		this->atlas_enemy_idle = atlas;
+	}
+
+	void set_position(POINT position) {
+		this->enemy_position = position;
+	}
+
+	void set_animation(const Animation& animation) {
+		this->animation_enemy_idle = animation;
+	}
+
+	void dead() {
+		this->is_alive = false;
+	}
+
+	bool is_dead() const {
+		return !is_alive;
+	}
+
+	Animation& animation() {
+		return this->animation_enemy_idle;
+	}
+
+	POINT& position() {
+		return this->enemy_position;
+	}
+
+
+
+private:
+	Animation animation_enemy_idle;
+	Atlas* atlas_enemy_idle;
+	POINT enemy_position;
+	bool is_alive = true;
+};
+
+
+
+#endif // _ENEMTY_H_
--- 代码空间/VS2022/game_name/player.h
+++ 代码空间/VS2022/game_name/player.h
@@ -19,6 +19,10 @@ public:
 	void set_position(int x, int y) {
 		this->x = x;
 		this->y = y;
+		this->target_x = x;
+		this->target_y = y;
+		player_position.x = 120 + x * 40;
+		player_position.y = 40 + y * 40;
 	}
 
 	void set_atlas(Atlas* atlas) {
@@ -34,42 +38,76 @@ public:
 		case WM_KEYDOWN:
 			switch (msg.vkcode) {
 			case VK_UP:
-				is_move_up = true; 
+				move_direction = Direction::UP;
 				break;
 			case VK_DOWN:
-				is_move_down = true;
+				move_direction = Direction::DOWN;
 				break;
 			case VK_LEFT:
-				is_move_left = true; 
+				move_direction = Direction::LEFT;
 				break;
 			case VK_RIGHT:
-				is_move_right = true;
+				move_direction = Direction::RIGHT;
 				break;
 			}
 			break;
-
 		case WM_KEYUP:
-			switch (msg.vkcode) {
-			case VK_UP:
-				is_move_up = false; 
-				break;
-			case VK_DOWN:
-				is_move_down = false; 
-				break;
-			case VK_LEFT:
-				is_move_left = false; 
-				break;
-			case VK_RIGHT:
-				is_move_right = false;
-				break;
-			}
+			move_direction = Direction::NONE;
 			break;
 		}
 	}
 
 	virtual void data_update(int delta) {
-		player_position.x = 120 + x * 40;
-		player_position.y = 40 + y * 40;
+		//更新input内容的映射
+		if (move_direction != Direction::NONE) {
+			target_x = x;
+			target_y = y;
+			switch (move_direction) {
+			case Direction::UP:
+				if (y > 0) target_y--;
+				break;
+			case Direction::DOWN:
+				if (y < 15) target_y++;
+				break;
+			case Direction::LEFT:
+				if (x > 0) target_x--;
+				break;
+			case Direction::RIGHT:
+				if (x < 25) target_x++;
+				break;
+			}
+			move_direction = Direction::NONE;
+			is_moving = true;
+			game_map[x][y] = 0;					 //重置原来位置
+		}
+		//更新player位置
+		if (is_moving) {
+			int target_position_x = 120 + target_x * 40;
+			int target_position_y = 40 + target_y * 40;
+			if (player_position.x < target_position_x) {
+				player_position.x += speed * delta;
+				if (player_position.x > target_position_x) player_position.x = target_position_x;
+			}
+			else if (player_position.x > target_position_x) {
+				player_position.x -= speed * delta;
+				if (player_position.x < target_position_x) player_position.x = target_position_x;
+			}
+			else if (player_position.y < target_position_y) {
+				player_position.y += speed * delta;
+				if (player_position.y > target_position_y) player_position.y = target_position_y;
+			}
+			else if (player_position.y > target_position_y) {
+				player_position.y -= speed * delta;
+				if (player_position.y < target_position_y) player_position.y = target_position_y;
+			}
+			//如果到达目标位置，停止移动
+			if (player_position.x == target_position_x && player_position.y == target_position_y) {
+				is_moving = false;
+				x = target_x;
+				y = target_y;
+				game_map[x][y] = 1;					//更新新位置
+			}
+		}
 
 	}
 
@@ -77,27 +115,27 @@ public:
 		animation_player_idle->picture_draw(player_position.x, player_position.y);
 	}
 
-protected:
-	void Move() {
-
-	}
+private:
+	enum class Direction {
+		NONE,
+		UP,
+		DOWN,
+		LEFT,
+		RIGHT
+	};
 
 private:
-	int x, y;			//标定map[x][y]的位置
+	int x, y;					//标定map[x][y]的位置
+	int speed = 15;				//标定移动速度
+	int target_x, target_y;		//目标位置
+	bool is_moving = false;		//标定是否在移动`
+
+	Direction move_direction = Direction::NONE;
 
 
 private:
 	Atlas* atlas_player_idle;
 	Animation* animation_player_idle;
-
-
-private:
-	bool is_move_up = false;
-	bool is_move_down = false;
-	bool is_move_left = false;
-	bool is_move_right = false;
-
-
 
 };
 
