c077885487f1cbba3bbaf2e6dd0e82922d22fb5f
Douvahkiin <ppllaayyeerrwatasu@gmail.com>
2024-08-27 16:17:44 +0800

    修改图片

--- 代码空间/VS2022/game_name/item.h
+++ 代码空间/VS2022/game_name/item.h
@@ -10,37 +10,55 @@
 extern int game_map[14][12];
 extern Atlas atlas_coral;
 extern Atlas atlas_star;
+extern Atlas atlas_bubble;
+extern Atlas atlas_flow;
+extern Atlas atlas_machanic_trigger; // 机关触发器
+extern Atlas atlas_machanic_gate; // 机关门
+extern Atlas atlas_penetration_wall; // 可穿越墙壁
 
 class Item {
 public:
-	Item()=default;
-	~Item()=default;
+  Item() = default;
+  ~Item() = default;
 
-	virtual void date_update(int delta) {
+  virtual void date_update(int delta) {
 
-	}
+  }
 
-	virtual void picture_draw(int x,int y) {
+  virtual void picture_draw(int x, int y) {
 
-	}
+  }
+
+  virtual void picture_draw() {}
+
+  void set_position(POINT point) {
+    position = point;
+  }
+
 
 protected:
-	Animation  animation_item_idle;			//动画对象
+  Animation  animation_item_idle;			//动画对象
+  POINT position;
 
 };
 
 class Coral :public Item {
 public:
-	Coral() {
-		animation_item_idle.set_atlas(&atlas_coral);
-		
+  Coral(POINT position) {
+    this->position = position;
+    animation_item_idle.set_atlas(&atlas_coral);
 
-	}
-	~Coral()=default;
 
-	void picture_draw(int x,int y) {
-		animation_item_idle.picture_draw(80 * x + 80, y * 50 + 60);
-	}
+  }
+  ~Coral() = default;
+
+  void picture_draw(int x, int y) {
+    animation_item_idle.picture_draw(80 * x + 80, y * 50 + 60);
+  }
+
+  void picture_draw() {
+    animation_item_idle.picture_draw(80 * position.x + 80, 50 * position.y + 60);
+  }
 
 
 
@@ -48,21 +66,50 @@ public:
 
 class Star :public Item {
 public:
-	Star() {
-		animation_item_idle.set_atlas(&atlas_star);
+  Star(POINT position) {
+    this->position = position;
+    animation_item_idle.set_atlas(&atlas_star);
 
 
-	}
-	~Star() = default;
+  }
+  ~Star() = default;
 
-	void picture_draw(int x, int y) {
-		animation_item_idle.picture_draw(80 * x + 95, y * 50 + 58);
-	}
+  void picture_draw(int x, int y) {
+    animation_item_idle.picture_draw(80 * x + 95, 50 * y + 58);
+  }
+
+  void picture_draw() {
+    animation_item_idle.picture_draw(80 * position.x + 95, 50 * position.y + 58);
+  }
 
 
 
 };
 
+class Bubble :public Item {
+public:
+  Bubble(POINT position) {
+    this->position = position;
+    animation_item_idle.set_atlas(&atlas_bubble);
+  }
+  ~Bubble() = default;
+
+};
+
+class Penetration_wall :public Item {
+public:
+  Penetration_wall(POINT position) {
+    this->position = position;
+    animation_item_idle.set_atlas(&atlas_penetration_wall);
+  }
+  ~Penetration_wall() = default;
+};
+
+class Mechanic_trigger :public Item {};
+
+class Mechanic_gate :public Item {};
+
+class Flow :public Item {};
 
 
 
